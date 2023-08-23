2c9c5bfc6398e196564c7bb8f05479f10aed0048
caiorochadev <139146069+caiorochadev@users.noreply.github.com>
2023-08-23 14:26:10 -0300

    Código resolvido

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,56 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
+class Genero(val genero: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Inscricao(val usuario: Usuario, val genero: Genero)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var nivel : Nivel) {
+
+    val inscritos = mutableListOf<Inscricao>()
+   
+    fun matricular(usuarios: Usuario, generos: Genero) {
+        val inscricao = Inscricao(usuarios, generos)
+        inscritos.add(inscricao)
+
+        println("Usuário ${usuarios.nome} (g:${generos.genero}), matriculado(a) na formação $nome")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
+//conteudos
+val conteudosKotlin = listOf(
+    	ConteudoEducacional("Introdução", 20),
+        ConteudoEducacional("Funcoes",40),
+        ConteudoEducacional("Assincronismo",60)
+)
+val conteudosAndroid = listOf(
+    	ConteudoEducacional("Introdução", 10),
+        ConteudoEducacional("View",30),
+        ConteudoEducacional("XML completo",50)
+)
+
+//formações
+
+val formacaoKotlin = Formacao("kotlin",conteudosKotlin, Nivel.DIFICIL)
+val formacaoAndroid = Formacao("Android",conteudosAndroid, Nivel.INTERMEDIARIO)
+
+//usuarios
+val Caio = Usuario("Caio")
+val Adriele = Usuario("Adriele")
+
+//genero
+val feminino = Genero("feminino")
+val masculino = Genero("masculino")
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    formacaoKotlin.matricular(Caio, masculino)
+    formacaoAndroid.matricular(Adriele, feminino)
+    //formacaoAndroid.matricular(usuario2)
+    
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
